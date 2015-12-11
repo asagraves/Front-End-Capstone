7e00a1f93c9adfb02d8eb70e06b152fef7f66217
Andrew Sagraves <andrewsagraves@Andrews-MacBook-Pro.local>
2015-12-11 11:31:02 -0600

    login/out initial working, need to get it to where user has to log in each time

--- capstone/app/controllers/authUser.js
+++ capstone/app/controllers/authUser.js
@@ -1,41 +1,69 @@
-app.controller("authUser", ["$scope", "$firebaseArray",  
-  function($scope, $firebaseArray){
-
-  var ref = new Firebase("https://cheveux-pure.firebaseio.com");
-  ref.authWithOAuthPopup("google", function(error, authData) {
-    if (error) {
-      console.log("Login Failed!", error);
-    } else {
-      console.log("Authenticated successfully with payload:", authData);
-
-      var userList = $firebaseArray(ref.child("users"));
-
-      userList.$loaded()
-      .then(function(data){
-        console.log("data,", data);
-
-        for(var i = 0; i < data.length; i++){
-
-          if(data[i].userUid === authData.uid){
-            
-            break
-
-          } else {
-            
-          ref.child("users").child(authData.uid).set({
-            "userUid" : authData.uid
-          })
-
-          }
-        }
-      });
+app.controller("authUser", ["$scope", "$firebaseArray", "logging", 
+  function($scope, $firebaseArray, logging){
+    var ref = new Firebase("https://cheveux-pure.firebaseio.com");
 
 
-    }
-  });
+
+     // Logout user
+    $scope.logout = function() {
+      console.log("Preparing to log user out");
+
+      var ref = new Firebase("https://cheveux-pure.firebaseio.com/" + logging.getUid() + "/");
+        console.log("lisapertee", logout);
+      ref.unauth();
+      location.reload();
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }]);
+  // var ref = new Firebase("https://cheveux-pure.firebaseio.com");
+  // ref.authWithOAuthPopup("google", function(error, authData) {
+  //   if (error) {
+  //     console.log("Login Failed!", error);
+  //   } else {
+  //     console.log("Authenticated successfully with payload:", authData);
+
+      // var userList = $firebaseArray(ref.child("users"));
+
+      // userList.$loaded()
+      // .then(function(data){
+      //   console.log("data,", data);
+
+      //   for(var i = 0; i < data.length; i++){
+
+      //     if(data[i].userUid === authData.uid){
+
+      //       break
+
+      //     } else {
+            
+      //     ref.child("users").child(authData.uid).set({
+      //       "userUid" : authData.uid
+      //     })
+
+      //     }
+      //   }
+      // });
+
+
+  //   }
+  // });
+
+
 
 
 
--- capstone/app/controllers/mainPage.js
+++ capstone/app/controllers/mainPage.js
@@ -1,13 +1,38 @@
-app.controller("mainPage", ["$scope", "$firebaseArray", 
-  function($scope, $firebaseArray){
-    var ref = new Firebase("https://cheveux-pure.firebaseio.com/");
+app.controller("mainPage", ["$scope", "$firebaseArray", "logging",
+  function($scope, $firebaseArray, logging){
+    console.log("hey ", logging);
+    var ref = new Firebase("https://cheveux-pure.firebaseio.com");
+    // var logout = ("#logout");
+
+     var ref = new Firebase("https://cheveux-pure.firebaseio.com");
+      ref.authWithOAuthPopup("google", function(error, authData) {
+        if (error) {
+          console.log("Login Failed!", error);
+        } else {
+          console.log("Authenticated successfully with payload:", authData);
+          logging.setUid(authData.uid);
+          console.log(logging.getUid());
+        }
+      })
+
+
 
     // Logout user
-    logout.click(function() {
-      var ref = new Firebase("https://cheveux-pure.firebaseio.com/" + uid + "/");
+    $scope.logout = function() {
+      var ref = new Firebase("https://cheveux-pure.firebaseio.com/" + logging.getUid() + "/");
         console.log("lisapertee", logout);
       ref.unauth();
       location.reload();
-    });
+    };
+
+
+
+
+
+
+
+
 
 }]);
+
+
