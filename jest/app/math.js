3373e9950e51a80cfc6628454cc2a2da83a0aee9
Andrew Sagraves <andrewsagraves@Andrews-MBP.attlocal.net>
2015-12-13 16:07:49 -0500

    12/13

---
+++ jest/app/math.js
@@ -0,0 +1,24 @@
+var module = module || {};
+
+function sum(one, two) {
+  return one + two;
+}
+
+function difference(five, two) {
+  return five - two;
+}
+
+function product(two, five) {
+  return two * five;
+}
+
+function quotient(ten, two) {
+  return ten / two;
+}
+
+
+
+
+
+
+module.exports = { sum, difference, product, quotient };
